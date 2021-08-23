93d6cd2a4fd9e10410827e066e7d808f4fce9c01
RealGuillaume <RealGuillaume@users.noreply.github.com>
2021-08-23 17:09:05 -0400

    first commit

---
+++ jstoolchain/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [{
+    enabled: true,
+    content: ['../snipcartwagtaildemo/templates/**/*.html'],
+  }
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
